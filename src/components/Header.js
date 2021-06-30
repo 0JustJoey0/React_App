e7ddd626733be2c132235d6e94a6f0597c8b9da9
unknown <you@example.com>
2021-06-30 12:09:45 -0400

    Got Header up and running

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import logo from "../Photos/ICON.png"
+
+function Header() {
+    return (
+        <div id="header-wrapper">
+            <img src = {logo} alt ="Logo" />
+            <div id="text-wrapper">
+                <h1>JUST JOEY</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
