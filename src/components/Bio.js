b19b2cc81b7b8727dd75c106793625067ae2db4b
Joey Dowds <the.jo3.man@gmail.com>
2021-07-02 06:34:17 -0400

    got bio done

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,13 +1,15 @@
 function Bio() {
     return (
         <div className = "bio-wrapper">
-            <h1>ABOUT ME</h1>
+            <div className="bio-block">
+            <h1 className="bio-title">ABOUT ME</h1>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                 aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
+            </div>
         </div>
     )
 }
