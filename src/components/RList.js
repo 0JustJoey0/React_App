f646e42934c9aaa2596fc861202ac18e60ef73d9
unknown <you@example.com>
2021-07-04 09:38:04 -0400

    bootstrap

--- src/components/RList.js
+++ src/components/RList.js
@@ -1,17 +1,49 @@
-import Books from "./Books"
-import arrow from "../Photos/arrow.svg"
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import photo from "../Photos/IMG_0934.jpg"
 
 
 
 function RList() {
     return (
-        <div className="reading-wrapper">
-            <Books className="Book1"/>
-            <Books className="Book2"/>
-            <Books className="Book3"/>
-            <Books className="Book4"/>
-            <img className="arrow" src={arrow} alt="arrow"/>
-        </div>
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={photo}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
     )
 }
 
